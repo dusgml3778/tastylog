0fa174e5f3e449667020d7ee77dc7348156a5a92
kimmingye <dusgml3779@gmail.com>
2023-11-06 21:42:16 +0900

    tastylog upload

---
+++ app.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const PORT = process.env.PORT;
+const path = require("path");
+const logger = require("./lib/log/logger.js");
+const accesslogger = require("./lib/log/accesslogger.js");
+const applicationlogger = require("./lib/log/applicationlogger.js");
+const express = require("express");
+const favicon = require("serve-favicon");
+const app = express();
+
+
+//expressでejsを使うための設定
+app.set("view engine", "ejs");
+app.disable("x-powered-by");
+
+// Expose global method to view engine
+app.use((req, res, next) => {
+  res.locals.moment = require("moment"); 
+  res.locals.padding = require("./lib/math/math.js").padding;
+  next();
+}); 
+// static resource rooting
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+// Set access log
+app.use(accesslogger());
+// Dynamic resource rooting
+app.use("/", require("./routes/index.js"));
+app.use("/test", async (req, res, next) => {
+
+  const {
+    MySQLClient,
+    sql
+  } = require("./lib/database/client.js");
+  var data;
+
+  try {
+    data = await MySQLClient.executeQuery(await sql("SELECT_SHOP_BASIC_BY_ID"), [1]);
+    console.log(data);
+  } catch (err) {
+    next(err);
+  } 
+  res.end("OK");
+});
+
+// Set application log
+app.use(applicationlogger());
+
+// Excute web application
+app.listen(PORT, () => {
+  logger.application.info(`Server is ${PORT}`);
+});
